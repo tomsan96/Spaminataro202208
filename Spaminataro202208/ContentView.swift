05d9dd0afc84f492f4fe3692c769920ff937d799
tomsan96 <gradada0507@gmail.com>
2022-08-18 09:55:24 +0900

    first commit

---
+++ Spaminataro202208/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spaminataro202208
+//
+//  Created by 山崎定知 on 2022/08/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spaminataro202208/Spaminataro202208App.swift
@@ -0,0 +1,17 @@
+//
+//  Spaminataro202208App.swift
+//  Spaminataro202208
+//
+//  Created by 山崎定知 on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct Spaminataro202208App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
