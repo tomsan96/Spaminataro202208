3723a1680f9c66e04785db50cc33d3fc4d4618d2
naoki-mrnk <20cm0135@jec.ac.jp>
2022-08-21 11:07:52 +0900

    不要なプリントの削除

--- Spaminataro202208/Screen/Home/CultivationgViewModel.swift
+++ Spaminataro202208/Screen/Home/CultivationgViewModel.swift
@@ -41,7 +41,6 @@ class CultivationgViewModel: ObservableObject {
                 quantityType: distanceType,
                 quantitySamplePredicate: predicate,
                 options: [.cumulativeSum]) { _, statistics, _ in
-                    print(statistics!.sumQuantity()!)
                     if let quantity = statistics?.sumQuantity() {
                         let stepValue = quantity.doubleValue(for: HKUnit.count())
 
